dc3b048ea812d48de4cf2bf61deca824d36e8860
Jesse Horne <j.horne2796@gmail.com>
2019-09-28 20:49:19 -0500

    fixed bug

--- lib/customizer.php
+++ lib/customizer.php
@@ -42,9 +42,9 @@ function wc_add_long_description() {
 	global $product;
 
 	?>
-        <div itemprop="description">
+        <!-- <div itemprop="description">
             <?php // echo apply_filters( 'the_content', $product->post->post_content ) ?>
-        </div>
+        </div> -->
 	<?php
 }
 
