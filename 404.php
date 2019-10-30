31fa64d09555623dfa3e27d0262f1c7d05ba8a30
tony <tony@hotcookiebakery.com>
2019-10-30 14:24:51 -0700

    Update 404.php
    Again

--- 404.php
+++ 404.php
@@ -6,8 +6,14 @@
                 <center class="page-content">
                     <br>
                     <br>
-                    <h2><?php _e( 'Oops, that cookie page does not exist', 'hotcookie' ); ?></h2>
-                    <a href="<?php echo get_home_url(); ?>"> <?php _e( 'Try Hot Cookie home', 'hotcookie' ); ?></a>
+                    <h2><?php _e( 'Oops, lost our cookies', 'hotcookie' ); ?></h2>
+                    <h4><?php _e( 'What could have caused this?', 'hotcookie' ); ?></h4>
+                    <p><?php _e( 'We removed a page as part of our website redesign', 'hotcookie' ); ?></p>
+                    <p><?php _e( 'The link you clicked might be old and does not work anymore', 'hotcookie' ); ?></p>
+                    <p><?php _e( 'You might have accidentally typed an incorrectly URL', 'hotcookie' ); ?></p>
+                    <h4><?php _e( 'What can you do?', 'hotcookie' ); ?></h4>
+                    <p><a href="<?php echo get_home_url(); ?>"> <?php _e( 'Go to Hot Cookie home', 'hotcookie' ); ?></a></p>
+                    <p><a href="<?php echo get_home_url(); ?>/gift-boxes"> <?php _e( 'Send yourself or a friend a gift', 'hotcookie' ); ?></a></p>
                     <br>
                     <br>
                 </center><!-- .page-content -->
