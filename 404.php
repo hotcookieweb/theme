9225a892a5b86deb0003aa6a9939af143120e207
tony <tony@hotcookiebakery.com>
2019-10-30 14:10:05 -0700

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,7 +1,18 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
-</div>
+<div id="primary" class="content-area">
+        <div id="content" class="site-content" role="main">
+            <div class="page-wrapper">
+                <center class="page-content">
+                    <br>
+                    <br>
+                    <h2><?php _e( 'Oops, that cookie page does not exist', 'hotcookie' ); ?></h2>
+                    <a href="<?php echo get_home_url(); ?>"> <?php _e( 'Try Hot Cookie home', 'hotcookie' ); ?></a>
+                    <br>
+                    <br>
+                </center><!-- .page-content -->
+            </div><!-- .page-wrapper -->
+        </div><!-- #content -->
+    </div><!-- #primary -->
 
-<?php get_search_form(); ?>
+<?php get_template_part('templates/page', 'footer'); ?>
