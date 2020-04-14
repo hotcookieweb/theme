c1d2bd49709c201ba3023a2ec38a67fe5e9b1c2c
tony <tony@hotcookiebakery.com>
2020-04-14 14:38:25 -0700

    add coupon and zip save

--- functions.php
+++ functions.php
@@ -27,12 +27,42 @@ foreach ($sage_includes as $file) {
 }
 unset($file, $filepath);
 
-/* assume billing address is shipping address unless customer checks different */
-add_filter( 'woocommerce_ship_to_different_address_checked', '__return_true');
-
+// add_action('get_header', 'maintenance_mode');
 function maintenance_mode() {
 
       if ( !current_user_can( 'edit_themes' ) || !is_user_logged_in() ) {wp_die("Sorry, we're baking some updates. They'll be out of the oven soon!");}
 
 }
-// add_action('get_header', 'maintenance_mode');
+
+/* assume billing address is shipping address unless customer checks different */
+add_filter( 'woocommerce_ship_to_different_address_checked', '__return_true');
+
+// always start a woocommerce session when first page loads
+add_action( 'woocommerce_init', 'create_wc_session');
+function create_wc_session() {
+  if (is_user_logged_in() || is_admin())
+    return;
+  if (isset(WC()->session)) {
+    if (!WC()->session->has_session()) {
+      WC()->session->set_customer_session_cookie(true);
+      WC()->customer->set_shipping_postcode ( "94114" );
+    }
+  }
+}
+
+add_action('init', 'get_custom_coupon_code_to_session');
+function get_custom_coupon_code_to_session(){
+    if( isset($_GET['coupon_code']) ){
+        $coupon_code = esc_attr( $_GET['coupon_code'] );
+        WC()->session->set( 'coupon_code', $coupon_code ); // Set the coupon code in session
+    }
+}
+
+add_action( 'woocommerce_cart_coupon', 'add_coupon_to_cart', 10, 0 );
+function add_coupon_to_cart( ) {
+    // Set coupon code
+    $coupon_code = WC()->session->get('coupon_code');
+    if ( ! empty( $coupon_code )) {
+      echo '<script>  coupon_code.value="' . $coupon_code . '"</script>';
+   }
+}
