f97011df6221a4c18e147873ffcf7d48396e5c02
tony <tony@hotcookiebakery.com>
2020-06-07 21:02:17 -0700

    changed "the-bakery" to "our-store"
    Allows changing all blog posts to be under /blog/post

--- templates/components/store-categories.php
+++ templates/components/store-categories.php
@@ -1,30 +1,30 @@
 <ul class="shop-categories">
 	<li>
-		<a href="<?= esc_url(home_url('/the-bakery/cookies/')); ?>" title="#">
+		<a href="<?= esc_url(home_url('/our-store/cookies/')); ?>" title="#">
 			<img src="<?php echo get_bloginfo('template_directory'); ?>/assets/images/Cookies-Home.png" alt="Cookies" />
 			<h2>Cookies</h2>
 		</a>
 	</li>
 	<li>
-		<a href="<?= esc_url(home_url('/the-bakery/famous')); ?>" title="#">
+		<a href="<?= esc_url(home_url('/our-store/famous')); ?>" title="#">
 			<img src="<?php echo get_bloginfo('template_directory'); ?>/assets/images/Famous-Home.png" alt="Famous" />
 			<h2>Famous</h2>
 		</a>
 	</li>
 	<li>
-		<a href="<?= esc_url(home_url('/the-bakery/treats')); ?>" title="#">
+		<a href="<?= esc_url(home_url('/our-store/treats')); ?>" title="#">
 			<img src="<?php echo get_bloginfo('template_directory'); ?>/assets/images/Treat-Home.png" alt="Treats" />
 			<h2>Treats</h2>
 		</a>
 	</li>
 	<li>
-		<a href="<?= esc_url(home_url('/the-bakery/drinks')); ?>" title="#">
+		<a href="<?= esc_url(home_url('/our-store/drinks')); ?>" title="#">
 			<img src="<?php echo get_bloginfo('template_directory'); ?>/assets/images/milk-glass.png" alt="Drinks" />
 			<h2>Drinks</h2>
 		</a>
 	</li>
 	<li>
-		<a href="<?= esc_url(home_url('/the-bakery/gear')); ?>" title="#">
+		<a href="<?= esc_url(home_url('/our-store/gear')); ?>" title="#">
 			<img src="<?php echo get_bloginfo('template_directory'); ?>/assets/images/product-4-300x275.png" alt="Gear" />
 			<h2>Gear</h2>
 			<p>Complete your Hot Cookie experience with our famous Hot Cookie underwear and t-shirt. Buy our gear (underwear, t-shirt or both), post a picture on snapchat, instagram or facebook and we’ll send you a gift certificate for a free cookie or treat.</p>
