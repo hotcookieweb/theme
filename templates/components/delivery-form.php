c2def4b066c4169bf5fbde3d0ed0f39b7e20fb28
tony <tony@hotcookie.com>
2023-08-03 14:58:45 -0700

    get zips from shipping zones

--- templates/components/delivery-form.php
+++ templates/components/delivery-form.php
@@ -1,21 +1,21 @@
 
-	<input autocomplete="off" class="frontpage-input" maxlength="5" id="hc-address-input" placeholder="Enter destination ZIP (e.g 94114)" type="text" pattern="[0-9]{5}" value="">
+	<?php
+		$data_store = WC_Data_Store::load('shipping-zone');
+		$raw_zones = $data_store->get_zones();
+		foreach ( $raw_zones as $raw_zone ) {
+			$zone = new WC_Shipping_Zone($raw_zone);
+			$locations[$zone->get_zone_name()] = $zone->get_formatted_location(1000);
+		}
+		?>
+	<input autocomplete="off" class="frontpage-input" maxlength="5" id="hc-zip-input" placeholder="Enter US destination ZIP (e.g 94114)" type="text" pattern="[0-9]{5}" value="">
+
 	<button class="icon-search" href="#" id="hc-submit" title="HotCookie Search">Search</button>
 
 	<script>
 
 	var hc_submit = document.getElementById("hc-submit");
 
-	var hc_input = document.getElementById("hc-address-input");
-
-	serialize = function(obj) {
-		var str = [];
-		for (var p in obj)
-		if (obj.hasOwnProperty(p)) {
-			str.push(encodeURIComponent(p) + "/" + encodeURIComponent(obj[p]));
-		}
-		return str.join("&");
-	}
+	var hc_input = document.getElementById("hc-zip-input");
 
 	hc_input.addEventListener("keyup", function(e) {
 		if (e.keyCode == 13) {
@@ -25,21 +25,27 @@
 
 	hc_submit.addEventListener("click", function() {
 		var zipCodePattern = /^\d{5}$/;
-		var zipcode = document.getElementById("hc-address-input").value;
-		var url = "home";
+		var zipcode = hc_input.value;
+		var url = "home/";
+		var zonelast = '';
 		if (zipCodePattern.test(zipcode)) {
-			var castro_zips = ["94122","94116","94132","94117","94114","94131","94127","94112","94134","94110","94124"];
-			var polk_zips = ["94129","94123","94115","94109","94133","94111","94104","94105","94108","94102","94103","94107","94121","94118","94158","94101"];
-
-			if (castro_zips.includes(zipcode)) {
-				url = "delivery/castro-sf/";
-			} else if (polk_zips.includes(zipcode)) {
-				url = "delivery/polk-sf/";
-			} else {
-				url = "delivery/national/";
+			var locations = <?= json_encode($locations) ?>;
+      Object.entries(locations).forEach(([zone, location], index, arr) => {
+        console.log(zone, location);
+				if (location.includes(zipcode)) {
+					url = "delivery/" + zone;
+				}
+				zonelast = zone;
+			});
+			if (url == "home/") {
+				url = "delivery/" + zonelast; /* should  be national */
 			}
 			url += "?zipcode=" + zipcode;
+			window.location.href = url;
 		}
-		window.location.href = url;
+		else {
+			hc_input.value = "";
+		}
 	});
-	</script>
+
+</script>
