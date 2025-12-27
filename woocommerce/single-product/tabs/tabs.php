79ac546737cbd26c1a0d8faf1fc73431c741fd3e
tony <tony@hotcookie.com>
2025-12-27 11:44:25 -0800

    Update themes

--- woocommerce/single-product/tabs/tabs.php
+++ woocommerce/single-product/tabs/tabs.php
@@ -1,31 +1,50 @@
 <?php
 /**
- * Single Product tabs (clean separation version)
+ * Single Product tabs
  *
- * Copy to yourtheme/woocommerce/single-product/tabs/tabs.php
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/tabs/tabs.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 9.8.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+/**
+ * Filter tabs and allow third parties to add their own.
+ *
+ * Each tab is an array containing title, callback and priority.
+ *
+ * @see woocommerce_default_product_tabs()
+ */
 $product_tabs = apply_filters( 'woocommerce_product_tabs', array() );
 
 if ( ! empty( $product_tabs ) ) :
     if ( wp_is_mobile() ) : ?>
         <div class="woocommerce-tabs wc-tabs-wrapper">
-            <ul class="tabs wc-tabs">
-                <?php foreach ( $product_tabs as $key => $tab ) : ?>
-                    <li class="<?php echo esc_attr( $key ); ?>_tab">
-                        <a href="#tab-<?php echo esc_attr( $key ); ?>">
-                            <?php echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', esc_html( $tab['title'] ), $key ); ?>
+		<ul class="tabs wc-tabs" role="tablist">
+			<?php foreach ( $product_tabs as $key => $product_tab ) : ?>
+				<li role="presentation" class="<?php echo esc_attr( $key ); ?>_tab" id="tab-title-<?php echo esc_attr( $key ); ?>">
+					<a href="#tab-<?php echo esc_attr( $key ); ?>" role="tab" aria-controls="tab-<?php echo esc_attr( $key ); ?>">
+						<?php echo wp_kses_post( apply_filters( 'woocommerce_product_' . $key . '_tab_title', $product_tab['title'], $key ) ); ?>
                         </a>
                     </li>
                 <?php endforeach; ?>
             </ul>
-            <?php foreach ( $product_tabs as $key => $tab ) : ?>
-                <div id="tab-<?php echo esc_attr( $key ); ?>" class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr( $key ); ?>">
-                    <?php call_user_func( $tab['callback'], $key, $tab ); ?>
+		<?php foreach ( $product_tabs as $key => $product_tab ) : ?>
+			<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr( $key ); ?> panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>" role="tabpanel" aria-labelledby="tab-title-<?php echo esc_attr( $key ); ?>">
+				<?php
+            		call_user_func( $product_tab['callback'], $key, $product_tab );
+                ?>
                 </div>
             <?php endforeach; ?>
         </div>
