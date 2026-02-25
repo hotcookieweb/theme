6d203427ebff356ada02389e21a6d7794bf759f3
tony <tony@hotcookie.com>
2026-02-25 08:35:15 -0800

    Fix to display all products when store not selected

--- templates/components/products-list.php
+++ templates/components/products-list.php
@@ -1,12 +1,24 @@
-<?php if (get_field('product_category')) {
-    $category = get_field('product_category');
-    $sort_by = get_field('sort_by');
-    $columns = get_field('columns');
-    $limit = get_field_object('limit');
-    $limit_value = isset($limit['value']) ? $limit['value'] : 12;
-    $current_store = WC()->session->get('current_zone');
-    if (get_field('display_products') && !empty($category->slug)) {
-        echo do_shortcode('[products columns="' . esc_attr($columns) . '" category="' . esc_attr($category->slug) . '" orderby="' . esc_attr($sort_by) . '" cat_operator="AND" limit="' . esc_attr($limit_value) . '" paginate="true" current_store="' . esc_attr($current_store) . '"]');
-    }
-};
+<?php 
+if (get_field('product_category')) {
 
+    $category      = get_field('product_category');
+    $sort_by       = get_field('sort_by');
+    $columns       = get_field('columns');
+    $limit_value   = get_field('limit') ?: -1; 
+    $current_store = WC()->session->get('current_zone');
+    if (empty($current_store)) {
+        $current_store = 'any-zone';
+    }
+
+    if (get_field('display_products') && !empty($category->slug)) {
+
+        echo do_shortcode(
+            '[products columns="' . esc_attr($columns) . 
+            '" category="' . esc_attr($category->slug) . 
+            '" orderby="' . esc_attr($sort_by) . 
+            '" cat_operator="AND" limit="' . esc_attr($limit_value) . 
+            '" paginate="true" current_store="' . esc_attr($current_store) . '"]'
+        );
+    }
+}
+?>
