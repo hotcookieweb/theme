56d20ce65d38fa66f05d5849708206cf0a63bee2
tony <tony@hotcookie.com>
2026-06-11 08:32:55 -0700

    turn off all automatic updated

--- functions.php
+++ functions.php
@@ -780,3 +780,8 @@ add_action('admin_notices', function() {
     }
 });
 
+
+// Disable all automatic updates
+add_filter( 'auto_update_core', '__return_false' );
+add_filter( 'auto_update_plugin', '__return_false' );
+add_filter( 'auto_update_theme', '__return_false' );
